d7043abdef3d20a449467ddcf66d2e925ab128bf
BagasKuning <dwibagaskara55@gmail.com>
2024-02-09 19:11:08 +0700

    init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+export const Navbar = () => {
+    return (
+        <navbar className="flex max-w-[1200px]">
+            <div className="">
+                Akbar.Dev
+            </div>
+
+            <div>
+                <a href="#">Works</a>
+                <a href="#">About</a>
+                <a href="#">Resume</a>
+            </div>
+        </navbar>
+    )
+}
+
+export default Navbar;
