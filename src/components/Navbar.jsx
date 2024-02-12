fba422a267bafd92d26ba366ddbb7ddd5bc9d4f3
BagasKuning <dwibagaskara55@gmail.com>
2024-02-12 20:05:57 +0700

    deploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 export const Navbar = () => {
   return (
     <navbar className="absolute top-0 left-1/2 -translate-x-1/2 w-full flex m-auto max-w-[1250px] h-10 items-center justify-between">
       <div className="italic text-lg">Akbar.Dev</div>
 
       <div className="flex gap-3">
-        <a href="#">Works</a>
-        <a href="#">About</a>
-        <a href="#">Resume</a>
+        <a href="#works">Works</a>
+        <a href="#about">About</a>
+        <a href="#resume">Resume</a>
       </div>
     </navbar>
   );
