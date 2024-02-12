be1453c6474d51694f84f2de71874f8b0f6d1bb3
BagasKuning <dwibagaskara55@gmail.com>
2024-02-12 20:37:32 +0700

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@
 export const Navbar = () => {
   return (
     <navbar className="absolute top-0 left-1/2 -translate-x-1/2 w-full flex m-auto max-w-[1250px] h-10 items-center justify-between">
-      <div className="italic text-lg">Akbar.Dev</div>
+      <div className="italic sm:text-lg text-base">Akbar.Dev</div>
 
-      <div className="flex gap-3">
+      <div className="flex sm:gap-3 gap-2 sm:text-base text-sm">
         <a href="#works">Works</a>
         <a href="#about">About</a>
         <a href="#resume">Resume</a>
